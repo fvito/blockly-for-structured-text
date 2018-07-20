6b9433cd1ccf80aecf99de930ae5792675c02ef1
Vito Ferš <fvito@users.noreply.github.com>
2018-07-20 12:16:52 +0200

    Added the code output for all of the function blocks

--- js/generators/structured_text/function_blocks.js
+++ js/generators/structured_text/function_blocks.js
@@ -8,39 +8,61 @@ Blockly.ST['function_block_ton'] = function (block) {
     var name = block.getFieldValue('NAME');
     var enabled = Blockly.ST.valueToCode(block, "EN", Blockly.ST.ORDER_NONE) || 'FALSE';
     var time = Blockly.ST.valueToCode(block, "PT", Blockly.ST.ORDER_NONE) || 'TIME#0s';
-    return name+"(EN:="+enabled+", PT:="+time+")";
+    return name+"(IN:="+enabled+", PT:="+time+")";
 };
+Blockly.ST['function_block_tof'] = Blockly.ST['function_block_ton'];
+Blockly.ST['function_block_tp'] = Blockly.ST['function_block_ton'];
 
-Blockly.ST['function_block_tof'] = function (block) {
-    return 'TOF';
-};
-
-Blockly.ST['function_block_tp'] = function (block) {
-    return 'TP';
-};
 Blockly.ST['function_block_sr'] = function (block) {
-    return 'SR';
+    var name = block.getFieldValue('NAME');
+    var set = Blockly.ST.valueToCode(block, 'SET', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var reset = Blockly.ST.valueToCode(block, 'RESET', Blockly.ST.ORDER_NONE) || 'FALSE';
+    return  name+"(SET1:="+set+" ,RESET:="+reset+")";
 };
 Blockly.ST['function_block_rs'] = function (block) {
-    return 'RS';
+    var name = block.getFieldValue('NAME');
+    var set = Blockly.ST.valueToCode(block, 'SET', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var reset = Blockly.ST.valueToCode(block, 'RESET', Blockly.ST.ORDER_NONE) || 'FALSE';
+    return  name+"(SET:="+set+" ,RESET1:="+reset+")";
 };
+
 Blockly.ST['function_block_ctu'] = function (block) {
-    return 'CTU';
+    var name = block.getFieldValue('NAME');
+    var cu = Blockly.ST.valueToCode(block, 'CU', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var reset = Blockly.ST.valueToCode(block, 'RESET', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var pv = Blockly.ST.valueToCode(block, 'PV', Blockly.ST.ORDER_NONE) || '0';
+
+    return name+"(CU:="+cu+" ,RESET:="+reset+" ,PV:="+pv+")";
 };
 Blockly.ST['function_block_ctd'] = function (block) {
-    return 'CTD';
+    var name = block.getFieldValue('NAME');
+    var cd = Blockly.ST.valueToCode(block, 'CD', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var load = Blockly.ST.valueToCode(block, 'LOAD', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var pv = Blockly.ST.valueToCode(block, 'PV', Blockly.ST.ORDER_NONE) || '0';
+
+    return name+"(CD:="+cd+" ,LOAD:="+load+" ,PV:="+pv+")";
 };
 Blockly.ST['function_block_ctud'] = function (block) {
-    return 'CTUD';
+    var name = block.getFieldValue('NAME');
+    var cu = Blockly.ST.valueToCode(block, 'CU', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var reset = Blockly.ST.valueToCode(block, 'RESET', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var cd = Blockly.ST.valueToCode(block, 'CD', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var load = Blockly.ST.valueToCode(block, 'LOAD', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var pv = Blockly.ST.valueToCode(block, 'PV', Blockly.ST.ORDER_NONE) || '0';
+    return name+"(CU:="+cu+" ,CD:="+cd+" ,RESET:="+reset+" ,LOAD:="+load+" ,PV:="+pv+")";
 };
 Blockly.ST['function_block_f_trig'] = function (block) {
-    return 'F_TRIG';
+    var name = block.getFieldValue('NAME');
+    var clk = Blockly.ST.valueToCode(block, 'CLK', Blockly.ST.ORDER_NONE) || 'FALSE';
+    return name+"(CLK:="+clk+")";
 };
-Blockly.ST['function_block_r_trig'] = function (block) {
-    return 'R_TRIG';
-};
+Blockly.ST['function_block_r_trig'] = Blockly.ST['function_block_f_trig'];
+
 Blockly.ST['function_block_rtc'] = function (block) {
-    return 'RTC';
+    var name = block.getFieldValue('NAME');
+    var en = Blockly.ST.valueToCode(block, 'EN', Blockly.ST.ORDER_NONE) || 'FALSE';
+    var pdt = Blockly.ST.valueToCode(block, 'PDT', Blockly.ST.ORDER_NONE) || '0';
+    return name+"(EN:="+en+" ,PDT:="+pdt+")";
 };
 
 Blockly.ST['function_block_get'] = function (block) {
